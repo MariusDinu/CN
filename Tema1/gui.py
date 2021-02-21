f57a66e9f8158a4b4225c8106255afa33d0188ea
Marius Dinu <dinumariusciprian@gmail.com>
2021-02-21 15:21:41 +0200

    Ex 3 B

--- Tema1/gui.py
+++ Tema1/gui.py
@@ -1,10 +1,15 @@
+#Dinu Marius 3A5
+#Nassar Mahmoud 3A5
+#Tema 1
+#Calcule Numerice
+
 from tkinter import ttk
 
 try:
     from Tkinter import *
 except ImportError:
     from tkinter import *
-import test
+import main
 from PIL import Image, ImageTk
 
 
@@ -12,16 +17,16 @@ def start_gui():
     global val, w, root
     root = Tk()
     root.resizable(False, False)
-    top = Notepad_gui(root)  # aici sa seteza setarile pt layout
-    test.init(root, top)
+    top = CN_gui(root)  # aici sa seteza setarile pt layout
+    main.init(root, top)
     root.mainloop()
 
 
 w = None
 
-firstPrecision = test.minimnumber()
+firstPrecision = main.minimnumber()
 
-class Notepad_gui:
+class CN_gui:
     def __init__(self, top=None):
         _backgroundColor = '#969696'
         _foregroundColor = '#000000'
@@ -59,7 +64,11 @@ class Notepad_gui:
 
         self.TopNotebook_t3 = Frame(self.TopNotebook)
         self.TopNotebook.add(self.TopNotebook_t3, padding=3)
-        self.TopNotebook.tab(3, text="TEMA1 EX.3", compound="none", underline="-1", )
+        self.TopNotebook.tab(3, text="TEMA1 EX.3.A", compound="none", underline="-1", )
+
+        self.TopNotebook_t4 = Frame(self.TopNotebook)
+        self.TopNotebook.add(self.TopNotebook_t4, padding=3)
+        self.TopNotebook.tab(4, text="TEMA1 EX.3.B", compound="none", underline="-1", )
 
         self.LabelCreate = Label(self.TopNotebook_t0)
         self.LabelCreate.place(relx=0.050, rely=0.30, height=30, width=500)
@@ -71,7 +80,7 @@ class Notepad_gui:
         self.LabelCreate.place(relx=0.050, rely=0.60, height=30, width=500)
         self.LabelCreate.configure(foreground="#9F009F")
         self.LabelCreate.configure(font=("Courier", 30))
-        self.LabelCreate.configure(text=test.minimnumber())
+        self.LabelCreate.configure(text=main.minimnumber())
 
         self.LabelCreate = Label(self.TopNotebook_t1)
         self.LabelCreate.place(relx=0.050, rely=0.30, height=30, width=500)
@@ -83,19 +92,37 @@ class Notepad_gui:
         self.LabelCreate.place(relx=0.050, rely=0.60, height=30, width=500)
         self.LabelCreate.configure(foreground="#9F009F")
         self.LabelCreate.configure(font=("Courier", 10))
-        self.LabelCreate.configure(text=test.neasociativeOp(firstPrecision))
+        self.LabelCreate.configure(text=main.neasociativeOp(firstPrecision))
 
         self.LabelCreate = Label(self.TopNotebook_t2)
         self.LabelCreate.place(relx=0.050, rely=0.30, height=30, width=500)
         self.LabelCreate.configure(foreground="#9F009F")
         self.LabelCreate.configure(font=("Courier", 15))
-        self.LabelCreate.configure(text="Produsul este neasociativa pentru A,B,C:")
+        self.LabelCreate.configure(text="Produsul este neasociativ pentru A,B,C:")
 
         self.LabelCreate = Label(self.TopNotebook_t2)
         self.LabelCreate.place(relx=0.050, rely=0.60, height=30, width=500)
         self.LabelCreate.configure(foreground="#9F009F")
         self.LabelCreate.configure(font=("Courier", 10))
-        self.LabelCreate.configure(text=test.exProdusNeasociativ(firstPrecision))
+        self.LabelCreate.configure(text=main.exProdusNeasociativ(firstPrecision))
+
+        self.LabelCreate = Label(self.TopNotebook_t3)
+        self.LabelCreate.place(relx=0.050, rely=0.30, height=30, width=500)
+        self.LabelCreate.configure(foreground="#9F009F")
+        self.LabelCreate.configure(font=("Courier", 10))
+        self.LabelCreate.configure(text="urmeaza")
+
+        self.LabelCreate = Label(self.TopNotebook_t4)
+        self.LabelCreate.place(relx=0.050, rely=0.30, height=30, width=500)
+        self.LabelCreate.configure(foreground="#9F009F")
+        self.LabelCreate.configure(font=("Courier", 10))
+        self.LabelCreate.configure(text="Eroare medie  Timp de calcul")
+
+        self.LabelCreate = Label(self.TopNotebook_t4)
+        self.LabelCreate.place(relx=0.050, rely=0.60, height=30, width=500)
+        self.LabelCreate.configure(foreground="#9F009F")
+        self.LabelCreate.configure(font=("Courier", 10))
+        self.LabelCreate.configure(text=main.runTan())
 
 if __name__ == '__main__':
     start_gui()
--- Tema1/main.py
+++ Tema1/main.py
@@ -1,8 +1,20 @@
+#Dinu Marius 3A5
+#Nassar Mahmoud 3A5
+#Tema 1
+#Calcule Numerice
+
+
 def init(top, gui, *args, **kwargs):
     global w, top_level, root
     w = gui
     top_level = top
     root = top
+
+
+import math
+import random
+import time
+
 
 def minimnumber():
     # am creat un array ca sa salvam toate numerele u care respecta conditiile
@@ -37,25 +49,86 @@ def neasociativeOp(u):
     if (a * b) * c != a * (b * c):
         print("Produsul este neasociativ")
         print(a, b, c)
-    return a,b,c
+    return a, b, c
+
 
 def exProdusNeasociativ(u):
-
-    a=2.2
-    b=u/10
-    c=u/10
+    a = 2.2
+    b = u / 10
+    c = u / 10
     if (a * b) * c != a * (b * c):
         print("Produsul este neasociativ")
-        print ("Primul produs:",(a*b)*c)
-        print("Al doilea produs:",a * (b * c))
-    return a,b,c
+        print("Primul produs:", (a * b) * c)
+        print("Al doilea produs:", a * (b * c))
+    return a, b, c
+
+
+def method2Tan(number):
+    c1 = 0.33333333333333333
+    c2 = 0.133333333333333333
+    c3 = 0.053968253968254
+    c4 = 0.0218694885361552
+
+    x1 = number ** 3
+    x2 = number ** 5
+    x3 = number ** 7
+    x4 = number ** 9
+
+    polinom = number + c1 * x1 + c2 * x2 + c3 * x3 + c4 * x4
+    return polinom
+
+
+def runTan():
+    print("Method 2: Polinoms")
+    # preluam valoare pi
+    pi = math.pi
+    # salvam timpul de start
+    startTime = time.time()
+    sumError = 0
+
+    #avel cele 10000 de variabile din for
+    for i in range(0, 10000):
+        myTan = 0
+        pyTan = 0
+        #luam variabile random uniform din intervalul nostru
+        value = random.uniform(-pi / 2, pi / 2)
+
+        if -pi / 4 < value < pi / 4:
+            myTan = method2Tan(value)
+            pyTan = math.tan(value)
+
+        if pi / 4 < value < pi / 2:
+            myTan = 1 / (method2Tan((pi / 2) - value))
+            pyTan = 1 / (math.tan((pi / 2) - value))
+
+        if -pi / 2 < value < -pi / 4:
+            myTan = 1 / (method2Tan(pi - ((pi / 2) - value)))
+            pyTan = 1 / (math.tan(pi - ((pi / 2) - value)))
+        #calculam suma de la eroarea medie
+        sumError = sumError + abs(pyTan - myTan)
+        print("[", i + 1, "]", "Pentru x = ", value, ", Eroare medie =", abs(pyTan - myTan))
+
+    avgErr = sumError / 10000
+    print("Formula eroare medie: (|tan(x)−my tan(x)|) ")
+    print("Eroarea medie de calcul:", format(avgErr, ".12f"))
+
+    finalTime = time.time()
+    print("Timpul de calcul :", (finalTime - startTime))
+
+    return format(avgErr, ".12f"),(finalTime - startTime)
 
 
 if __name__ == '__main__':
-    #Ex1
+
+    # Ex1
     firstPrecision = minimnumber()
-    #Ex2 a.
+
+    # Ex2 a.
     neasociativeOp(firstPrecision)
-    #Ex2 b.
+    # Ex2 b.
     exProdusNeasociativ(firstPrecision)
-    #Ex3
+
+    # Ex3 method1: Continuous Fractions
+
+    # Ex3 method2: Polinoms
+    runTan()
