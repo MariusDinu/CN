e53ffc5d06cf59548b6e901c875eebfcd9610c42
Marius Dinu <dinumariusciprian@gmail.com>
2021-02-21 14:01:13 +0200

    ex2 back

--- Tema1/main.py
+++ Tema1/main.py
@@ -1,28 +1,48 @@
-
-
 def minimnumber():
-    #am creat un array ca sa salvam toate numerele u care respecta conditiile
+    # am creat un array ca sa salvam toate numerele u care respecta conditiile
     numbers = []
     for m in range(1000):
-        #declaram si initializam u unde m este de la 0 la 1000
+        # declaram si initializam u unde m este de la 0 la 1000
         u = 1 / pow(10, m)
-        #verificam prima conditie
+        # verificam prima conditie
         if 1.0 + u != 1.0:
-            #verificam daca u este mai mare ca 0
+            # verificam daca u este mai mare ca 0
             if u > 0:
-                #inseram in array u
-                numbers.insert(m,u)
-            #afisam fiecare u care respecta conditiile
+                # inseram in array u
+                numbers.insert(m, u)
+            # afisam fiecare u care respecta conditiile
             print(u)
     print(numbers)
-    #sortam array-ul ca sa avem pe prima pozitie cel mai mic numar
+    # sortam array-ul ca sa avem pe prima pozitie cel mai mic numar
     numbers.sort()
-    #afisam numarul cel mai mic din array
+    # afisam numarul cel mai mic din array
     print(numbers[0])
+    return numbers[0]
 
 
-# Press the green button in the gutter to run the script.
+def neasociativeOp(u):
+    a = 1.0
+    b = u / 10
+    c = u / 10
+    if (a + b) + c != a + (b + c):
+        print("Suma este neasociativa:")
+        print(a, b, c)
+
+    if (a * b) * c != a * (b * c):
+        print("Produsul este neasociativ")
+        print(a, b, c)
+
+def exProdusNeasociativ():
+    a=1.999*2.3
+    b=2.000
+    c=2.000
+    if (a * b) * c != a * (b * c):
+        print("Produsul este neasociativ")
+        print(a, b, c)
+
 if __name__ == '__main__':
-    minimnumber()
+    #firstPrecision = minimnumber()
+    #neasociativeOp(firstPrecision)
+    exProdusNeasociativ()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
